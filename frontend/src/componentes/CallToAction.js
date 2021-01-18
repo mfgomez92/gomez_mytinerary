32a93081611f2cb554b0c4e5c16d9ba9ac1be3e6
Matias Gomez <mfgomez.810@gmail.com>
2021-01-18 11:31:42 -0300

    Eliminacion carpeta backend

--- frontend/src/componentes/CallToAction.js
+++ frontend/src/componentes/CallToAction.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const CallToAction =()=> { 
     return  <Container className="fondo ">
                 <Row className="justify-content-end px-5">
-                    <Col xs lg="4" className="centrado">
+                    <Col xs lg="4" className="centrado hover-lento">
                         <p  className="fs-3">Find your perfect trip, designed by insiders who know and love their cities</p>
                         <Link to="/cities">
                             <Button variant="secondary" size="lg" className="align-self-end" >
